eb5fee3ffba8c4025c74a4f03a3ca176aeeca210
azder <azhder@gmail.com>
2013-02-02 11:01:30 +0100

    Modified the git, java and tweetdeck installs and added ipt-flush and node.js ones

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get -y install -u git
 
-git config --global user.name "Your Name"
+git config --global user.name "azder"
 git config --global user.email azhder@gmail.com
--- tweetdeck.sh
+++ tweetdeck.sh
@@ -4,7 +4,16 @@
 sudo apt-get -y install lib32asound2 lib32gcc1 lib32ncurses5 lib32stdc++6 lib32z1 libc6 libc6-i386 lib32nss-mdns
 
 #files to get for install
-GETLIBS_FILE_URL=http://taurinocerveza.com/scripts/getlibs-all.deb
+GETLIBS_FILE=getlibs-all.deb
+GETLIBS_FILE_URL=http://taurinocerveza.com/scripts/${GETLIBS_FILE}
+
+AIR_FILE=AdobeAIRInstaller.bin
+AIR_FILE_URL=http://airdownload.adobe.com/air/lin/download/2.6/${AIR_FILE}
+
+#TweetDeck_0_37.5.air
+TWEETDECK_FILE=tweetdeck 
+TWEETDECK_FILE_URL=http://tweetdeck.com/go/download/tweetdeck
+
 
 #create a temporary directory to do the work in
 DIR=`mktemp --tmpdir -d getlibs-XXXXXX` || {
@@ -17,4 +26,17 @@ echo "Using the directory $DIR for downloading the installation file"
 cd $DIR
 wget $GETLIBS_FILE_URL
 
-sudo getlibs -l libnss3.so.1d libnssutil3.so.1d libsmime3.so.1d libssl3.so.1d libnspr4.so.0d libplc4.so.0d \  libplds4.so.0d libgnome-keyring.so libgnome-keyring.so.0 libgnome-keyring.so.0.1.1
+sudo -n dpkg -i $GETLIBS_FILE
+
+sudo -n getlibs -l libnss3.so.1d libnssutil3.so.1d libsmime3.so.1d libssl3.so.1d libnspr4.so.0d libplc4.so.0d \  libplds4.so.0d libgnome-keyring.so libgnome-keyring.so.0 libgnome-keyring.so.0.1.1
+
+
+wget $AIR_FILE_URL
+chmod +x $AIR_FILE
+sudo -n $AIR_FILE
+
+sudo -n ldconfig  
+
+wget $TWEETDECK_FILE_URL
+chmod +x $TWEETDECK_FILE
+./$TWEETDECK_FILE
